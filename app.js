16cee52a51a4a1536080e3d63e9e394162adaf03
Ubuntu <ubuntu@ip-172-31-34-85.ap-south-1.compute.internal>
2026-06-18 02:18:57 +0000

    Version 2 deployment test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello Mohan - CI/CD Project");
+  res.send("Version 2 Deployed Automatically");
 });
 
 app.listen(3000, () => {
